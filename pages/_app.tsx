e2a8103ce193b463b5db4f58f65dde9ad278d322
MincheolC <mccha0407@gmail.com>
2021-04-12 19:19:51 +0900

    fix: NextJS + Material UI class name mismatch problem

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { AppProps } from 'next/app'
 import CThemeProvider from '../components/utility/CThemeProvider'
 import CssBaseline from '@material-ui/core/CssBaseline'
@@ -7,10 +9,23 @@ export default function App({
   Component,
   pageProps,
 }: AppProps): React.ReactNode {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
   return (
     <CThemeProvider>
       <CssBaseline />
       <Component {...pageProps} />
     </CThemeProvider>
   )
+}
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
 }
